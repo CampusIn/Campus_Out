0640481fee27f2b47b62faf5478b32248b3dd4de
Joel <eapenjoel4@gmail.com>
2026-06-17 21:08:03 +0530

    testing

---
+++ test/sum.test.js
@@ -0,0 +1,13 @@
+const sum = require('./async');
+
+
+describe("Arrays",()=>{
+    it('arrays test',()=>{
+        const Arrays = [
+        "milk","Joel","Messi","Ronaldo"
+    ]
+
+    expect(Arrays).toContain('milk')
+    })
+    
+})
